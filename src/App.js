68eef13ca007bb6e5ec330979b801815ede76ca3
Harsh Tiwar <harsh.tiwar9@gmail.com>
2021-01-05 22:01:37 -0500

    Login Module with Bootstrap Modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Modal, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Cart from './components/Cart/Cart';
 import ProductList from './components/ProductList/ProductList';
 import { useDispatch } from 'react-redux';
@@ -9,10 +12,14 @@ const App = () => {
     const dispatch = useDispatch();
     const dbUrl = "http://localhost:3001";
     const [products, setProducts] = useState([]);
+    const [show, setShow] = useState(false);
+    const handleClose = () => { console.log(show); setShow(false) };
+    const handleShow = () => { console.log(show); setShow(true) };
 
-    function removeFromCart(id,name,stock, price, currency) {
+
+    function removeFromCart(id, name, stock, price, currency) {
         // Product.setIsInCart(false)
-    
+
         let data = {
             id: id,
             productNameInCart: name,
@@ -21,7 +28,7 @@ const App = () => {
             currency: currency
         }
 
-        axios.delete(dbUrl + "/cart/"+data.id)
+        axios.delete(dbUrl + "/cart/" + data.id)
             .then(function (response) {
                 // console.log(response.data)
                 if (response.data === true) {
@@ -48,7 +55,7 @@ const App = () => {
                             // console.log(Object.keys(response2.data))
                             // console.log(response2.data)
                             for (let i = 0; i < response2.data.length; i++) {
-                                dispatch({ 'type': 'addItem', data: response2.data[i]});
+                                dispatch({ 'type': 'addItem', data: response2.data[i] });
                             }
                             // console.log(data)
                             // dispatch({ 'type': 'addItem', data: data })
@@ -65,8 +72,42 @@ const App = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-12">
+                <div className="col-8 col-md-8">
                     <h1> Shopping Cart Example</h1>
+                </div>
+                <div className="col-4 col-md-4">
+                    <br />
+                    <Button variant="primary" onClick={handleShow}>
+                        Login
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose} animation={false}>
+                        <Modal.Header>
+                            <Modal.Title>Login</Modal.Title>
+                        </Modal.Header>
+                        <form>
+                            <Modal.Body>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                                </div>
+                                <div className="mb-3 form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <label className="form-check-label" for="exampleCheck1">Remember Me</label>
+                                </div>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button type="submit" variant="secondary">Login</Button>
+                                <Button type="reset" variant="primary">Reset</Button>
+                            </Modal.Footer>
+                        </form>
+                    </Modal>
+
                 </div>
             </div>
             <div className="row">
@@ -80,6 +121,7 @@ const App = () => {
             </div>
 
         </div>
+
     );
 }
 
